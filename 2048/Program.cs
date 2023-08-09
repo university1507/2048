7449a77d1b98a57b21342c0a678ad0af11d5edd4
MyStIkStIk <109526792+MyStIkStIk@users.noreply.github.com>
2023-08-09 20:29:08 +0300

    added part of control logic

--- 2048/Program.cs
+++ 2048/Program.cs
@@ -18,13 +18,14 @@ namespace _2048
 
         static void DrawMap()
         {
+            Console.Clear();
             for (int i = 0; i < map.GetLength(0); i++)
             {
                 for (int j = 0; j < map.GetLength(1); j++)
                 {
                     int length = map[i, j].ToString().Length;
                     string space = new string(' ', 4 - length);
-                    
+
                     Console.Write(space + map[i, j] + "|");
                 }
                 Console.WriteLine();
@@ -34,6 +35,7 @@ namespace _2048
 
         static void Main(string[] args)
         {
+            DrawMap();
             UpdateDirection();
         }
 
@@ -41,11 +43,87 @@ namespace _2048
         {
             while (true)
             {
-                direction = Control.GetKey(Console.ReadKey().Key);
-                Console.WriteLine(direction);
+                UpdateCellsPositions(Console.ReadKey().Key);
+                DrawMap();
             }
-            DrawMap();
-            Console.ReadKey();
+        }
+
+        static void UpdateCellsPositions(ConsoleKey inputKey)
+        {
+            switch (inputKey)
+            {
+                case ConsoleKey.LeftArrow:
+                case ConsoleKey.A:
+                    for (int i = 0; i < map.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < map.GetLength(1); j++)
+                        {
+                            if (j > 0)
+                            {
+                                map[i, j - 1] = map[i, j];
+                            }
+                            if (j == map.GetLength(1) - 1)
+                            {
+                                map[i, j] = 0;
+                            }
+                        }
+                    }
+                    break;
+
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.W:
+                    for (int i = 0; i < map.GetLength(1); i++)
+                    {
+                        for (int j = 0; j < map.GetLength(0); j++)
+                        {
+                            if (j > 0)
+                            {
+                                map[j - 1, i] = map[j, i];
+                            }
+                            if (j == map.GetLength(1) - 1)
+                            {
+                                map[j, i] = 0;
+                            }
+                        }
+                    }
+                    break;
+
+                case ConsoleKey.RightArrow:
+                case ConsoleKey.D:
+                    for (int i = 0; i < map.GetLength(0); i++)
+                    {
+                        for (int j = map.GetLength(1); j >= 0; j--)
+                        {
+                            if (j < map.GetLength(1) - 1)
+                            {
+                                map[i, j + 1] = map[i, j];
+                            }
+                            if (j == 0)
+                            {
+                                map[i, j] = 0;
+                            }
+                        }
+                    }
+                    break;
+
+                case ConsoleKey.DownArrow:
+                case ConsoleKey.S:
+                    for (int i = 0; i < map.GetLength(1); i++)
+                    {
+                        for (int j = map.GetLength(0); j >= 0; j--)
+                        {
+                            if (j < map.GetLength(0) - 1)
+                            {
+                                map[j + 1, i] = map[j, i];
+                            }
+                            if (j == 0)
+                            {
+                                map[j, i] = 0;
+                            }
+                        }
+                    }
+                    break;
+            }
         }
     }
 }
